c9ef5ff2727bac7060711d8660746b32f81c8c79
Armard Bellamy <armardbellamy@gmail.com>
2018-05-13 13:19:14 -0400

    Added Layout function and created higher ordered component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const Layout = (props) => (
+  <Aux>
+    <div>ToolBar, Sidedrawer, Backdrop</div>
+    <main>
+      {props.children}
+    </main>
+  </Aux>
+
+);
+
+export default Layout;
