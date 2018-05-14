516f96d3b5857f18ce59f7594248badd6bde1a6b
Armard Bellamy <armardbellamy@gmail.com>
2018-05-14 17:36:05 -0400

    Create and style individual build control

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './BuildControls.css';
+
+const BuildControls = (props) => {
+  return (
+    <div classname={classes.BuildControls}>
+    </div>
+  );
+}
+
+export default BuildControls;
