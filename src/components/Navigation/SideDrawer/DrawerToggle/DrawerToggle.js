245767ff022e40b939bb2dd3f96b2830539b77b8
Armard Bellamy <armardbellamy@gmail.com>
2018-05-15 23:26:16 -0400

    Add hamburger bar for side nav

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import classes from './DrawerToggle.css';
 const DrawerToggle = (props) => {
-  return (
-    <div onClick={props.clicked}>
-      MENU
-    </div>
-  );
+  return <div className={classes.DrawerToggle} onClick={props.clicked}>
+      <div />
+      <div />
+      <div />
+    </div>;
 };
 
 export default DrawerToggle;
