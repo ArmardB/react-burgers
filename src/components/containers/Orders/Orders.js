b22a867aeeaa87ba1bc2f111381b0c00c3e33284
Armard Bellamy <armard@Armards-MacBook-Pro.local>
2018-05-28 15:01:03 -0400

    Styled order items

--- src/components/containers/Orders/Orders.js
+++ src/components/containers/Orders/Orders.js
@@ -34,11 +34,14 @@ class Orders extends Component {
   }
 
 
+
+
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => (
+          <Order key={order.id} ingredients={order.ingredients} price={order.price}/>
+        ))}
       </div>
     )
   }
